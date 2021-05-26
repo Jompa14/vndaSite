adcfb649cb8dabc8871c6f736bb7e34588a43501
Jompa14 <joao.buchholz@gmail.com>
2021-05-25 22:38:04 -0300

    gulp-uglify instalado e funcionando

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,24 @@
+const gulp      = require('gulp');
+const uglifycss = require('gulp-uglifycss')
+
+
+gulp.task('uglifycss', function () {
+  gulp.src('*.css')
+    .pipe(uglifycss({
+      // "maxLineLen": 200,
+      "uglyComments": true
+    }))
+    .pipe(gulp.dest('./dist/'));
+});
+
+// quando houverem mais tasks podem ser add neste array:
+// gulp.task('run', ['uglifycss']);
+
+// gulp.task('watch', function() {
+//   gulp.watch('*.css', ['uglifycss'])
+// });
+
+// gulp.task('default', 'uglifycss');
 function defaultTask(cb) {
   // place code for your default task here
   cb();
